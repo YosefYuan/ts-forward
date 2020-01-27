799ccf48465865570f55f6a07d238436e661343f
Yosef Yuan <yuanruixiangjet@163.com>
2020-01-27 18:09:24 +0800

    feat: ch3-3 cheerio

--- ch2/src/crowller.ts
+++ ch2/src/crowller.ts
@@ -1,15 +1,43 @@
 // ts -> .d.ts 翻译文件 @types/superagent -> js
 
 import superagent from 'superagent';
+import cheerio from 'cheerio';
 
+interface Course {
+  title: string;
+  count: number;
+}
 class Crowller {
   // https://git.imooc.com/coding-412/source-code 密码更新地址
   private secret = 'secretKey';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
 
+  getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $('.course-item');
+    const courseInfos: Course[] = [];
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc');
+      const title = descs.eq(0).text();
+      const count = parseInt(
+        descs
+          .eq(1)
+          .text()
+          .split('：')[1],
+        10
+      );
+      courseInfos.push({ title, count });
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    };
+    console.log(result);
+  }
+
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    console.dir(result);
+    this.getCourseInfo(result.text);
   }
 
   constructor() {
