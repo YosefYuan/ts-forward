fd95deff5e3332485a80213a98d00a630526dc4e
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-03 12:03:23 +0800

    feat: 连合类型&类型保护

---
+++ ch4/demo.ts
@@ -0,0 +1,47 @@
+interface Bird {
+  fly: boolean;
+  sing: () => {};
+}
+
+interface Dog {
+  fly: boolean;
+  bark: () => {};
+}
+
+// 通过类型断言的方式
+function trainAnimal(animal: Bird | Dog) {
+  if (animal.fly) {
+    (animal as Bird).sing();
+  } else {
+    (animal as Dog).bark();
+  }
+}
+
+// in 语法 来做类型保护
+function trainAnimalSecond(animal: Bird | Dog) {
+  if ('sing' in animal) {
+    animal.sing();
+  } else {
+    animal.bark();
+  }
+}
+
+// typeof 语法 做类型保护
+function add(first: number | string, second: number | string) {
+  if (typeof first === 'string' || typeof second === 'string') {
+    return `${first}${second}`;
+  }
+  return first + second;
+}
+
+// instanceof 语法 做类型保护
+class NumberObj {
+  count: number;
+}
+
+function addSecond(first: object | NumberObj, second: object | NumberObj) {
+  if (first instanceof NumberObj && second instanceof NumberObj) {
+    return first.count + second.count;
+  }
+  return 0;
+}
