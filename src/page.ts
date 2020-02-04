81be579b78a0de26d16073b7e0239d40dd45c661
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-04 22:27:34 +0800

    feat: add keyof

---
+++ src/page.ts
@@ -0,0 +1,20 @@
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+class Teacher {
+  constructor(private info: Person) {}
+  getInfo<T extends keyof Person>(key: T): Person[T] {
+    return this.info[key];
+  }
+}
+
+const teacher = new Teacher({
+  name: 'yosef',
+  age: 30,
+  gender: 'male'
+});
+
+console.log(teacher.getInfo('name'));
