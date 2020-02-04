9e32b99c442ead79491a107cf1fda06f7e84aa19
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-04 20:03:41 +0800

    feat: ch9-import

---
+++ src/components.ts
@@ -0,0 +1,23 @@
+export class Header {
+  constructor() {
+    const Elem = document.createElement('div');
+    Elem.innerText = 'This is Header';
+    document.body.appendChild(Elem);
+  }
+}
+
+export class Content {
+  constructor() {
+    const Elem = document.createElement('div');
+    Elem.innerText = 'This is Content';
+    document.body.appendChild(Elem);
+  }
+}
+
+export class Footer {
+  constructor() {
+    const Elem = document.createElement('div');
+    Elem.innerText = 'This is Footer';
+    document.body.appendChild(Elem);
+  }
+}
