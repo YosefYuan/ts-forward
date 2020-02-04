5f8e43319de233fab992500f13a40319336eb421
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-04 21:07:14 +0800

    feat 4-12

--- src/jquery.d.ts
+++ src/jquery.d.ts
@@ -3,8 +3,23 @@
 
 // 定义全局函数
 interface jqueryInstance {
-  html: (html: string) => {};
+  html: (html: string) => jqueryInstance;
 }
-// 重载
+
+// 函数重载
 declare function $(readyFunc: () => void): void;
 declare function $(selector: string): jqueryInstance;
+
+// 对象-类型定义， 类-类型定义，命名空间的嵌套
+declare namespace $ {
+  namespace fn {
+    class init {}
+  }
+}
+
+// 使用interface语法实现函数重载
+// interface JQuery {
+//   (readyFunc: () => void): void;
+//   (selector: string): jqueryInstance;
+// }
+// declare var $: JQuery;
