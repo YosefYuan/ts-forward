df74927425de203002e63d0260f940e60c1459b1
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-04 22:06:43 +0800

    feat: 4-13es6

--- src/jquery.d.ts
+++ src/jquery.d.ts
@@ -1,25 +1,16 @@
-// 定义全局变量
-// declare var $: (params: () => void) => void;
-
-// 定义全局函数
-interface jqueryInstance {
-  html: (html: string) => jqueryInstance;
-}
-
-// 函数重载
-declare function $(readyFunc: () => void): void;
-declare function $(selector: string): jqueryInstance;
-
-// 对象-类型定义， 类-类型定义，命名空间的嵌套
-declare namespace $ {
-  namespace fn {
-    class init {}
+// ES6 模块化
+declare module 'jquery' {
+  interface jqueryInstance {
+    html: (html: string) => jqueryInstance;
   }
-}
 
-// 使用interface语法实现函数重载
-// interface JQuery {
-//   (readyFunc: () => void): void;
-//   (selector: string): jqueryInstance;
-// }
-// declare var $: JQuery;
+  // 混合类型
+  function $(readyFunc: () => void): void;
+  function $(selector: string): jqueryInstance;
+  namespace $ {
+    namespace fn {
+      class init {}
+    }
+  }
+  export = $;
+}
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,6 @@
+import $ from 'jquery';
+
 $(function() {
   $('body').html('<div>123</div>');
-  new $.fn.init();
+  // new $.fn.init();
 });
