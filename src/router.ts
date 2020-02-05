6c6f87663d8878633253fcfb46e51ae91093c2ac
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-05 15:02:52 +0800

    feat: 5-2 body-parser added

--- src/router.ts
+++ src/router.ts
@@ -4,15 +4,26 @@ import Crowller from './crowller';
 
 const router = Router();
 router.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+  res.send(`<html>
+  <body>
+    <form method='post' action='/getData'>
+      <input type='password' name='password' />
+      <button>提交</button>
+    </form>
+  <body>
+  </html>`);
 });
-router.get('/getData', (req: Request, res: Response) => {
-  // https://git.imooc.com/coding-412/source-code 密码更新地址
-  const secret = 'secretKey';
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-  const anylyzer = DellAnalyzer.getInstance();
-  new Crowller(url, anylyzer);
-  res.send('getData success');
+router.post('/getData', (req: Request, res: Response) => {
+  if (req.body.password === '123') {
+    // https://git.imooc.com/coding-412/source-code 密码更新地址
+    const secret = 'secretKey';
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+    const anylyzer = DellAnalyzer.getInstance();
+    new Crowller(url, anylyzer);
+    res.send('getData success');
+  } else {
+    res.send('getData Error');
+  }
 });
 
 export default router;
