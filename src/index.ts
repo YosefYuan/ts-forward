84f30c45a01aab9db7ed85339902c7dbb281d4a2
Yosef Yuan <yuanruixiangjet@163.com>
2020-02-05 18:10:36 +0800

    feat: 5-4 cookie-session added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
-import router from './router';
+import express from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import router from './router';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req: Request, res: Response, next: NextFunction) => {
-  req.studentName = 'yosef';
-  next();
-});
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['student yosef'],
+    maxAge: 24 * 3600 * 1000
+  })
+);
+
 app.use(router);
 
 app.listen(7001, () => {
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import DellAnalyzer from './dellAnalyzer';
 import Crowller from './crowller';
+import fs from 'fs';
+import path from 'path';
 
 const router = Router();
 interface CustomRequest extends Request {
@@ -10,17 +12,52 @@ interface CustomRequest extends Request {
 }
 
 router.get('/', (req: Request, res: Response) => {
-  res.send(`<html>
-  <body>
-    <form method='post' action='/getData'>
-      <input type='password' name='password' />
-      <button>提交</button>
-    </form>
-  <body>
-  </html>`);
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send(`<html>
+    <body>
+      <a href='/getData'>爬取内容</a>
+      <a href='/showData'>展示内容</a>
+      <a href='/logout'>退出</a>
+    <body>
+    </html>`);
+  } else {
+    res.send(`<html>
+    <body>
+      <form method='post' action='/login'>
+        <input type='password' name='password' />
+        <button>登录</button>
+      </form>
+    <body>
+    </html>`);
+  }
 });
-router.post('/getData', (req: CustomRequest, res: Response) => {
-  if (req.body.password === '123') {
+
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined;
+    res.redirect('/');
+  }
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { password } = req.body;
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send('already login');
+  } else {
+    if (password === '123' && req.session) {
+      req.session.login = true;
+      res.send('login success');
+    } else {
+      res.send('login fail');
+    }
+  }
+});
+
+router.get('/getData', (req: CustomRequest, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
     // https://git.imooc.com/coding-412/source-code 密码更新地址
     const secret = 'secretKey';
     const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
@@ -28,7 +65,22 @@ router.post('/getData', (req: CustomRequest, res: Response) => {
     new Crowller(url, anylyzer);
     res.send('getData success');
   } else {
-    res.send(`${req.studentName} getData Error`);
+    res.send('Please crawl content after login');
+  }
+});
+
+router.get('/showData', (req: CustomRequest, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    try {
+      const position = path.resolve(__dirname, '../data/course.json');
+      const content = fs.readFileSync(position, 'utf8');
+      res.send(content);
+    } catch {
+      res.send('crawl no content ever');
+    }
+  } else {
+    res.send('Please login in at first!');
   }
 });
 
